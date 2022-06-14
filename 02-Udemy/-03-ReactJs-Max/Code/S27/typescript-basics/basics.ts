00e7792763e0cdb4665e0352658bc531dbed4731
Abdolrahman EL-hagrasy <abdelrahmanelhagrasy661@gmail.com>
2022-06-15 00:01:42 +0200

    typescript basics

---
+++ 02-Udemy/-03-ReactJs-Max/Code/S27/typescript-basics/basics.ts
@@ -0,0 +1,25 @@
+let age: number;
+age = 16;
+
+let useName: string;
+useName = "Abdolrahman";
+
+let isDeveloper: boolean;
+isDeveloper = true;
+
+let hobbies: string[];
+hobbies = ["Sport"];
+
+let person: {
+  name: string;
+  age: number;
+};
+
+let persons: {
+  name: string;
+  age: number;
+}[];
+
+let course: string | number = "React";
+
+course = 123;
