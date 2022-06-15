225d63d196c8ab27d6538d7c6d8d6c8262715ee8
Abdolrahman EL-hagrasy <abdelrahmanelhagrasy661@gmail.com>
2022-06-15 10:51:48 +0200

    alias & functions & generics

--- 02-Udemy/-03-ReactJs-Max/Code/S27/typescript-basics/basics.ts
+++ 02-Udemy/-03-ReactJs-Max/Code/S27/typescript-basics/basics.ts
@@ -10,16 +10,35 @@ isDeveloper = true;
 let hobbies: string[];
 hobbies = ["Sport"];
 
-let person: {
+type Person = {
   name: string;
   age: number;
 };
 
-let persons: {
-  name: string;
-  age: number;
-}[];
+let person: Person;
+
+let persons: Person[];
 
 let course: string | number = "React";
 
 course = 123;
+
+// Functions
+
+function add(a: number, b: number) {
+  return a + b;
+}
+
+function printOut(value: any) {
+  console.log(value);
+}
+
+// Generics
+
+function insercAtBeginning<T>(array: T[], value: T) {
+  const newArray = [value, ...array];
+  return newArray;
+}
+
+const demoArray = [1, 2, 3];
+const updatedArray = insercAtBeginning(demoArray, 0);
