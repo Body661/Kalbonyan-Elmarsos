a5586c9ecbacff2883770d9b1683e8976bb0f705
Abdolrahman EL-hagrasy <abdelrahmanelhagrasy661@gmail.com>
2022-06-16 10:46:52 +0200

    Managing states & add styling

--- 02-Udemy/-03-ReactJs-Max/Code/S27/react-typescript/src/App.tsx
+++ 02-Udemy/-03-ReactJs-Max/Code/S27/react-typescript/src/App.tsx
@@ -1,17 +1,19 @@
 import Todo from "./models/todo";
 import Todos from "./components/Todos";
 import NewTodo from "./components/NewTodo";
-
-const todos = [
-  new Todo("Learn React"),
-  new Todo("Learn TypeScript"),
-  new Todo("Learn Nodejs"),
-];
+import { useState } from "react";
 
 function App() {
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const addTodoHandler = (todoText: string) => {
+    setTodos((prevTodo) => {
+      return [new Todo(todoText), ...prevTodo];
+    });
+  };
   return (
     <div>
-      <NewTodo />
+      <NewTodo addTodoHandler={addTodoHandler} />
       <Todos items={todos} />
     </div>
   );
--- 02-Udemy/-03-ReactJs-Max/Code/S27/react-typescript/src/components/NewTodo.tsx
+++ 02-Udemy/-03-ReactJs-Max/Code/S27/react-typescript/src/components/NewTodo.tsx
@@ -1,17 +1,20 @@
 import React, { useRef } from "react";
+import classes from "./NewTodo.module.css";
 
-const NewTodo = () => {
+const NewTodo: React.FC<{ addTodoHandler: (text: any) => void }> = (props) => {
   const inputRef = useRef<HTMLInputElement>(null);
 
   const formSubmitHandler = (e: React.FormEvent) => {
     e.preventDefault();
     const enteredText = inputRef.current?.value;
-    if (enteredText?.trim() !== "") {
-      console.log(enteredText);
+    if (enteredText?.trim().length === 0) {
+      return;
     }
+
+    props.addTodoHandler(enteredText);
   };
   return (
-    <form onSubmit={formSubmitHandler}>
+    <form onSubmit={formSubmitHandler} className={classes.form}>
       <label htmlFor="text">Todo title</label>
       <input type="text" id="text" ref={inputRef} />
       <button>Add todo</button>
