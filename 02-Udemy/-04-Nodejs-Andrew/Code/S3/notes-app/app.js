c9286e7000091d88e89bbf2f0bc24cac340dec4e
Abdolrahman EL-hagrasy <abdelrahmanelhagrasy661@gmail.com>
2022-06-20 23:54:13 +0200

    add notes.js

--- 02-Udemy/-04-Nodejs-Andrew/Code/S3/notes-app/app.js
+++ 02-Udemy/-04-Nodejs-Andrew/Code/S3/notes-app/app.js
@@ -1,5 +1,2 @@
-const fs = require('fs')
-
-fs.writeFileSync('notes.txt', 'This file is created by node js.')
-
-fs.appendFileSync('notes.txt', ' This message was appended')
+const getNotes = require('./notes')
+console.log(getNotes())
