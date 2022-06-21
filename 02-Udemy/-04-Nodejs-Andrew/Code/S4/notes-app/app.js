bf73ce4219a6550e002ad2f49acfe10316ef1ccd
Abdolrahman EL-hagrasy <abdelrahmanelhagrasy661@gmail.com>
2022-06-22 00:18:53 +0200

    add yargs

--- 02-Udemy/-04-Nodejs-Andrew/Code/S4/notes-app/app.js
+++ 02-Udemy/-04-Nodejs-Andrew/Code/S4/notes-app/app.js
@@ -1,10 +1,47 @@
-import validator from 'validator'
-import getNotes from './notes.js'
-import chalk from 'chalk'
+// import getNotes from './notes.js'
+// import chalk from 'chalk'
+// import yargs from 'yargs';
+//"type": "module",
 
-console.log(getNotes())
+const { command, version } = require("yargs");
+const yargs = require("yargs");
 
-console.log(validator.isEmail('text@test.com'))
 
-console.log(chalk.yellow.inverse.bold('Warning'))
+yargs.version('1.1.0')
 
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    builder: {
+        title: {
+            describe: 'Note Title'
+        }
+    },
+    handler: (argv) => {
+        console.log('adding new note' , argv.title)
+    }
+}).parse()
+
+yargs.command({
+    command: 'remove',
+    describe: 'remove note',
+    handler: () => {
+        console.log('Removing note')
+    }
+}).parse()
+
+yargs.command({
+    command: 'list',
+    describe: 'show list',
+    handler: () => {
+        console.log('List')
+    }
+}).parse()
+
+yargs.command({
+    command: 'read',
+    describe: 'read',
+    handler: () => {
+        console.log('read')
+    }
+}).parse()
