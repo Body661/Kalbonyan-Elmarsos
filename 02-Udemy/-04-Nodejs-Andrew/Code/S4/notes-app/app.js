c4f779ff4dbfb53d719db8585d7a2f48aec21920
Abdolrahman EL-hagrasy <abdelrahmanelhagrasy661@gmail.com>
2022-06-24 00:35:59 +0200

    notes app [FINISH]

--- 02-Udemy/-04-Nodejs-Andrew/Code/S4/notes-app/app.js
+++ 02-Udemy/-04-Nodejs-Andrew/Code/S4/notes-app/app.js
@@ -1,10 +1,7 @@
-// import getNotes from './notes.js'
-// import chalk from 'chalk'
-// import yargs from 'yargs';
-//"type": "module",
-
-const { command, version } = require("yargs");
+const fs = require('fs');
+const { title } = require('process');
 const yargs = require("yargs");
+const { addNote, removeNote, listNotes, readNote } = require('./notes');
 
 
 yargs.version('1.1.0')
@@ -14,34 +11,49 @@ yargs.command({
     describe: 'Add a new note',
     builder: {
         title: {
-            describe: 'Note Title'
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        },
+        body: {
+            describe: 'Add note body',
+            demandOption: true,
+            type: 'string'
         }
     },
-    handler: (argv) => {
-        console.log('adding new note' , argv.title)
-    }
-}).parse()
+    handler: (argv) => addNote(argv.title, argv.body)
+})
 
 yargs.command({
     command: 'remove',
     describe: 'remove note',
-    handler: () => {
-        console.log('Removing note')
-    }
-}).parse()
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: (argv) => removeNote(argv.title)
+})
 
 yargs.command({
     command: 'list',
     describe: 'show list',
-    handler: () => {
-        console.log('List')
-    }
-}).parse()
+    handler: () => listNotes()
+})
 
 yargs.command({
     command: 'read',
     describe: 'read',
-    handler: () => {
-        console.log('read')
-    }
-}).parse()
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: (argv) => readNote(argv.title)
+})
+
+yargs.parse()
