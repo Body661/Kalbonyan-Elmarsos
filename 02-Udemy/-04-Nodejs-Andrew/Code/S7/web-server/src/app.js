d6c498cb8028f2a0522a0c0111c70694a9c5e9c6
Abdolrahman EL-hagrasy <abdelrahmanelhagrasy661@gmail.com>
2022-06-28 17:36:12 +0200

    404 page & Partials & views path

--- 02-Udemy/-04-Nodejs-Andrew/Code/S7/web-server/src/app.js
+++ 02-Udemy/-04-Nodejs-Andrew/Code/S7/web-server/src/app.js
@@ -1,45 +1,59 @@
 const express = require('express')
 const app = express()
 const path = require('path')
+const hbs = require('hbs')
 
+// Define paths for Express config
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
+
+// setup handlebars engine and views location
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+// setup static directory to serve
 app.use(express.static(path.join(__dirname, '../public')))
+
 
 app.get('', (req, res) => {
     res.render('index', {
-        title: 'weather',
-        name: 'Abdolrahman'
+        title: 'Weather',
+        name: 'Abdolrahman Saleh'
     })
 })
 
 app.get('/about', (req, res) => {
     res.render('about', {
         title: 'About',
-        name: 'Abdolrahman'
+        name: 'Abdolrahman Saleh '
     })
 })
 
 app.get('/help', (req, res) => {
     res.render('help', {
-        helpText: 'Help'
+        title: 'Help',
+        helpText: 'Help',
+        name: 'Abdolrahman Saleh'
     })
 })
-
-app.set('view engine', 'hbs')
-
-// app.get('/help', (req, res) => {
-//     res.send({
-//         name: 'Abdolrahman'
-//     })
-// })
-
-// app.get('/about', (req, res) => {
-//     res.send('<h1>About</h1>')
-// })
 
 app.get('/weather', (req, res) => {
     res.send({
         forecast: '18 degrees',
         location: 'Amsterdam'
+    })
+})
+
+app.get('/help/*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        erorrMsg: 'Help article not found'
+    })
+})
+app.get('*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        erorrMsg: 'Page not found'
     })
 })
 
