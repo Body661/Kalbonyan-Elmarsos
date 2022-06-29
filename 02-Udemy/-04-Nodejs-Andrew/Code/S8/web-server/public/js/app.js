b2b04412d956a96292b1c000cda004c79e800812
Abdolrahman EL-hagrasy <abdelrahmanelhagrasy661@gmail.com>
2022-06-29 17:47:25 +0200

    fetching data with input field

--- 02-Udemy/-04-Nodejs-Andrew/Code/S8/web-server/public/js/app.js
+++ 02-Udemy/-04-Nodejs-Andrew/Code/S8/web-server/public/js/app.js
@@ -1,20 +1,25 @@
+const msg1 = document.querySelector('.message1')
+const weatherP = document.querySelector('.weather-response')
+
 const request = async (place) => {
+
     const response = await fetch(`/weather?address=${place}`)
     const data = await response.json()
 
     if (data.error) {
-        return console.log(data.error)
+        return msg1.innerHTML = data.error
     }
-
-    console.log(data.placeName)
-    console.log(data.termprature)
-    console.log(data.forecast)
-    
+    msg1.innerHTML = data.placeName + ' :'
+    weatherP.innerHTML = `${data.termprature} , ${data.forecast} `
 }
+
 const weatherForm = document.querySelector('form')
 const placeInput = document.querySelector('input')
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault()
+    msg1.innerHTML = 'Loading...'
+    weatherP.innerHTML = ''
+
     request(placeInput.value)
 })
 
