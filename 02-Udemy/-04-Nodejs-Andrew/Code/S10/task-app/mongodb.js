1ec1f157172ff0de3db840c9c08e299bad012ead
Abdolrahman EL-hagrasy <abdelrahmanelhagrasy661@gmail.com>
2022-07-02 16:12:38 +0200

    connecting to mongodb

---
+++ 02-Udemy/-04-Nodejs-Andrew/Code/S10/task-app/mongodb.js
@@ -0,0 +1,23 @@
+const MongoClient = require('mongodb').MongoClient;
+
+const connect = 'mongodb://127.0.0.1:27017'
+const databaseName = 'task-manager'
+
+MongoClient.connect(connect, { useNewUrlParser: true }, (error, client) => {
+    if (error) {
+        return console.log('Unable to connect to database')
+    }
+
+    const db = client.db(databaseName)
+    db.collection('users').insertOne({
+        name: 'abdolrahman',
+        age: 16
+    }, (err, result) => {
+        if (err) {
+            return console.log('Unable to insert user')
+        }
+
+        console.log(result.ops)
+    })
+
+})
