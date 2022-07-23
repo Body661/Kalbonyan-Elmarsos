c8a7d5a5a192c64a5302752bd962a11e9bc18afc
Abdolrahman EL-hagrasy <abdelrahmanelhagrasy661@gmail.com>
2022-07-23 17:50:57 +0200

    form and buttons state

--- 02-Udemy/-04-Nodejs-Andrew/Code/S17/chat-app/public/js/chat.js
+++ 02-Udemy/-04-Nodejs-Andrew/Code/S17/chat-app/public/js/chat.js
@@ -3,17 +3,7 @@ const socket = io()
 const msgForm = document.querySelector('.msg-form')
 const userMsg = document.querySelector('.msg')
 const msgSendBtn = document.querySelector('.msg-btn')
-
-// const incrementBtn = document.querySelector('.increment')
-
-// socket.on('countUpdate', (count) => {
-//     console.log('countUpdate completed ' + count)
-// })
-
-// incrementBtn.addEventListener('click', () => {
-//     socket.emit('increment')
-// })
-
+const locationBtn = document.querySelector('.location-btn')
 
 socket.on('message', (message) => {
     console.log(message)
@@ -21,7 +11,17 @@ socket.on('message', (message) => {
 
 msgForm.addEventListener('submit', (e) => {
     e.preventDefault()
+
+    if (userMsg.value.trim().length === 0) {
+        return console.log('Please enter a message')
+    }
+
+    msgSendBtn.setAttribute('disabled', 'disabled')
+
     socket.emit('sendMsg', userMsg.value, (error) => {
+        userMsg.value = ''
+        msgSendBtn.removeAttribute('disabled')
+        userMsg.focus()
 
         if (error) {
             return console.log(error)
@@ -29,20 +29,22 @@ msgForm.addEventListener('submit', (e) => {
 
         console.log('Message delivered successfully')
     })
-    userMsg.value = ''
 })
 
-const locationBtn = document.querySelector('.location-btn')
+
 locationBtn.addEventListener('click', () => {
     if (!navigator.geolocation) {
         return alert('Geolocation is not supported by your browser')
     }
+
+    locationBtn.setAttribute('disabled', 'disabled')
 
     navigator.geolocation.getCurrentPosition((position) => {
         socket.emit('sendLocation', {
             latitude: position.coords.latitude,
             longitude: position.coords.longitude
         }, () => {
+            locationBtn.removeAttribute('disabled')
             console.log('Location shared')
         })
     })
