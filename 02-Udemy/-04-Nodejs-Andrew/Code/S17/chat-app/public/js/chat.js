d1e81b5f770dc7d9501925e637a93fbf575d3c1f
Abdolrahman EL-hagrasy <abdelrahmanelhagrasy661@gmail.com>
2022-07-23 14:28:15 +0200

    sending messages

--- 02-Udemy/-04-Nodejs-Andrew/Code/S17/chat-app/public/js/chat.js
+++ 02-Udemy/-04-Nodejs-Andrew/Code/S17/chat-app/public/js/chat.js
@@ -1,1 +1,24 @@
-io()
+const socket = io()
+
+const msgForm = document.querySelector('.msg-form')
+const userMsg = document.querySelector('.msg')
+
+// const incrementBtn = document.querySelector('.increment')
+
+// socket.on('countUpdate', (count) => {
+//     console.log('countUpdate completed ' + count)
+// })
+
+// incrementBtn.addEventListener('click', () => {
+//     socket.emit('increment')
+// })
+
+
+socket.on('message', (message) => {
+    console.log(message)
+})
+
+msgForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    socket.emit('sendMsg', userMsg.value)
+})
--- 02-Udemy/-04-Nodejs-Andrew/Code/S17/chat-app/src/index.js
+++ 02-Udemy/-04-Nodejs-Andrew/Code/S17/chat-app/src/index.js
@@ -7,12 +7,28 @@ const app = express();
 const server = http.createServer(app)
 const io = socketio(server)
 
+
 const PORT = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, '../public')))
 
-io.on('connection', () => {
+
+io.on('connection', (socket) => {
     console.log('new connection')
+
+    // socket.emit('countUpdate', count)
+
+    // socket.on('increment', () => {
+    //     count++
+    // socket.emit('countUpdate', count)
+    //     io.emit('countUpdate', count)
+    // })
+
+    socket.emit('message', 'Welcome!')
+
+    socket.on('sendMsg', (message) => {
+        io.emit('message', message)
+    })
 })
 
 server.listen(PORT, () => {
