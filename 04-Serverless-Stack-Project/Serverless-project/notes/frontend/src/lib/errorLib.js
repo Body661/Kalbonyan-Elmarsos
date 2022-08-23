d7fbc7f4212086d6cdb50e8680f0953b64f3cd0e
Abdolrahman EL-hagrasy <abdelrahmanelhagrasy661@gmail.com>
2022-08-23 15:09:44 +0200

    Adding React error reporting

--- 04-Serverless-Stack-Project/Serverless-project/notes/frontend/src/lib/errorLib.js
+++ 04-Serverless-Stack-Project/Serverless-project/notes/frontend/src/lib/errorLib.js
@@ -2,7 +2,7 @@ import * as Sentry from "@sentry/browser";
 import config from "../config";
 import { useAlert } from 'react-alert'
 
-const isLocal = process.env.NODE_ENV === "development";
+const isLocal = false;
 
 export function initSentry() {
     if (isLocal) {
